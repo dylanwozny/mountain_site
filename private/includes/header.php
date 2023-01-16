248bfa5481577ff0a4b1f9ac18a72ac8574b82a1
dylan <dylanwozny14@gmail.com>
2023-01-15 23:12:28 -0700

    finished navbar mobile. continued styling and blocking out home page.

--- private/includes/header.php
+++ private/includes/header.php
@@ -45,7 +45,8 @@ if (!isset($page_title)) {
   <!-- Your Custom styles for this project -->
   <!--  Note how we can use WWW_ROOT constant to resolve all links no matter where the file resides. -->
   <!-- FOR DEVELOPMENT USE LOCAL BOOTSTRAP SO INTELLESENSE WORKS -->
-
+  <link href="<?php echo WWW_ROOT ?>/css/reset.css" rel="stylesheet">
+  <link href="<?php echo WWW_ROOT ?>/css/normalize.css" rel="stylesheet">
   <link href="<?php echo WWW_ROOT ?>/css/custom.min.css" rel="stylesheet">
 
 
@@ -86,7 +87,7 @@ if (!isset($page_title)) {
               </g>
             </svg>
           </a>
-          <a class="navbar-brand " href="<?php echo WWW_ROOT ?>/index.php">Canadian Mountains</a>
+          <a class="navbar-brand" href="<?php echo WWW_ROOT ?>/index.php">Canadian Mountains</a>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -94,46 +95,52 @@ if (!isset($page_title)) {
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class=" navbar-nav mr-auto w-100 align-items-center">
             <!-- WWW_ROOT is the root path constant -->
-            <li class="nav-item active">
-              <a class="nav-link" href="<?php echo WWW_ROOT ?>/index.php">Home</a>
+            <li class="nav-item active border-top  ">
+              <a class="nav-link" href="<?php echo WWW_ROOT ?>/index.php"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.281 22.77">
+                  <path d="M14.251,8.161,4.88,15.879V24.21a.813.813,0,0,0,.813.813l5.7-.015A.813.813,0,0,0,12.2,24.2V19.33a.813.813,0,0,1,.813-.813h3.253a.813.813,0,0,1,.813.813v4.862a.813.813,0,0,0,.813.816l5.694.016a.813.813,0,0,0,.813-.813V15.874l-9.37-7.713A.62.62,0,0,0,14.251,8.161Zm14.8,5.246-4.25-3.5V2.863a.61.61,0,0,0-.61-.61H21.349a.61.61,0,0,0-.61.61V6.554L16.188,2.81a2.44,2.44,0,0,0-3.1,0L.22,13.407a.61.61,0,0,0-.081.859l1.3,1.576a.61.61,0,0,0,.86.083L14.251,6.077a.62.62,0,0,1,.778,0l11.957,9.848a.61.61,0,0,0,.859-.081l1.3-1.576a.61.61,0,0,0-.086-.861Z" transform="translate(0.001 -2.254)" />
+                </svg>Home</a>
               <div>
                 <div></div>
               </div>
             </li>
-            <li class="d-flex justify-content-center align-items-center">
-              <a class="nav-link d-flex justify-content-center align-items-center" href="<?php echo WWW_ROOT ?>/search.php">Search</a>
-            </li>
-            <li>
-              <a class="nav-link" href="<?php echo WWW_ROOT ?>/canvas.php">Canvas</a>
-            </li>
-            <li>
-              <a class="nav-link" href="<?php echo WWW_ROOT ?>/larval.php">Larval</a>
-            </li>
             <?php if ((isset($_SESSION["x5ghy789soci"]))) { ?>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                  <a class="dropdown-item" href="<?php echo WWW_ROOT ?>/admin/index.php">menu</a>
+              <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.894 54.894">
+                    <path d="M4.5,35H28.9V4.5H4.5Zm0,24.4H28.9V41.1H4.5Zm30.5,0h24.4V28.9H35ZM35,4.5V22.8h24.4V4.5Z" transform="translate(-4.5 -4.5)" />
+                  </svg>
+                  Admin</a>
+                <div class="dropdown-menu p-0" aria-labelledby="dropdown01">
+                  <a class="dropdown-item" href="<?php echo WWW_ROOT ?>/admin/index.php">Dashboard</a>
                 </div>
               </li>
             <?php } ?>
             <!-- TODO: MAKE search into a reusable function and add to header -->
-            <li class="ms-auto">
-              <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </li>
-            <li>
+
+            <li class="nav-logout">
               <?php
               if (!(isset($_SESSION["x5ghy789soci"]))) {
-                echo "<a class=\"nav-link\" href=\"" . WWW_ROOT . "/admin/login.php\">Login</a>";
-                // echo "<p>" . session_id() . "</p>";
+
+                echo "<a class=\"nav-link\" href=\"" . WWW_ROOT . "/admin/login.php\">" ?> <svg viewBox="0 0 28 28">
+                  <path id="Icon_awesome-user-alt-2" data-name="Icon awesome-user-alt" d="M14,15.75A7.875,7.875,0,1,0,6.125,7.875,7.877,7.877,0,0,0,14,15.75Zm7,1.75H17.987a9.52,9.52,0,0,1-7.973,0H7a7,7,0,0,0-7,7v.875A2.626,2.626,0,0,0,2.625,28h22.75A2.626,2.626,0,0,0,28,25.375V24.5A7,7,0,0,0,21,17.5Z" />
+                </svg>
+                Login </a>
+              <?php
               } else {
-                echo "<a class=\"nav-link\" href=\"" . WWW_ROOT . "/admin/logout.php\">Logout</a>";
-                // echo "<p>" . session_id() . "</p>";
+                echo "<a class=\"nav-link\" href=\"" . WWW_ROOT . "/admin/logout.php\">" ?><svg viewBox="0 0 36 31.5">
+                  <path id="Icon_open-account-login-2" data-name="Icon open-account-login" d="M13.5,0V4.5h18V27h-18v4.5H36V0ZM18,9v4.5H0V18H18v4.5l9-6.75Z" />
+                </svg>Logout</a>
+              <?php
               }
               ?>
+            </li>
+            <li class="ms-auto  border-bottom nav-search pl-lg-4 pb-2 pt-2">
+              <form class="d-flex search-form ">
+                <input class="form-control me-2  search-form__input" type="search" placeholder="" aria-label="Search">
+                <svg role="img" viewBox="0 0 35.997 36.004">
+                  <path id="Icon_awesome-search-2" data-name="Icon awesome-search" d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z" />
+                </svg>
+                <button class="btn btn-outline-success search-form__button" type="submit">Search</button>
+              </form>
             </li>
           </ul>
       </nav>
--- public/index.php
+++ public/index.php
@@ -38,11 +38,11 @@ $result = find_all_mtns();
 </div>
 
 <!-- FINISH INTRO -->
-<header class="justify-content-between banner front-about-p d-flex flex-wrap">
-  <p class="banner__intro">Welcome to my Application ! I hope you find it somewhat useful. I wanted to create an app about my favourite feature of nature: Mountains !I have chosen to create a catalog of mountain peaks in Canada. I chose this topic because I love visiting Jasper and Banff and have been fascinated by mountains ever since I was a child. I also like to compare mountain stats like height, vertical relief etc. to each other just for fun.Information fields pertaining to the mountains will include: thumbnail picture, name, description,province, vertical relief, elevation profile, height, first summit, access type(vehicle, hiking, helicopter),and if it is a volcano and a google earth image of elevation. Search categories can include province,volcano etc. This information will be stored in a MySQL database.The structure of this application includes a useful homepage, search functionality, edit and delete options , which are secured by an admin log in and individual pages that display all information about that specific chosen mountain. Category links will also appear on the homepage.</p>
+<header class="justify-content-between banner front-about-p d-flex flex-wrap gap-5">
+  <p class="banner__intro flex-grow-0">Welcome to my Application ! I hope you find it somewhat useful. I wanted to create an app about my favourite feature of nature: Mountains !I have chosen to create a catalog of mountain peaks in Canada. I chose this topic because I love visiting Jasper and Banff and have been fascinated by mountains ever since I was a child. I also like to compare mountain stats like height, vertical relief etc. to each other just for fun.Information fields pertaining to the mountains will include: thumbnail picture, name, description,province, vertical relief, elevation profile, height, first summit, access type(vehicle, hiking, helicopter),and if it is a volcano and a google earth image of elevation. Search categories can include province,volcano etc. This information will be stored in a MySQL database.The structure of this application includes a useful homepage, search functionality, edit and delete options , which are secured by an admin log in and individual pages that display all information about that specific chosen mountain. Category links will also appear on the homepage.</p>
   <?php if (!(isset($_SESSION["x5ghy789soci"]))) { ?>
-    <div class="banner__login text-center flex-grow-0 rounded-3 shadow p-5 align-self-start">
-      <svg id="Icon_open-account-login" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 36 31.5">
+    <div class="banner__login text-center flex-grow-1 rounded-3 shadow p-5 align-self-start">
+      <svg class="mb-3" id="Icon_open-account-login" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 31.5">
         <path id="Icon_open-account-login-2" data-name="Icon open-account-login" d="M13.5,0V4.5h18V27h-18v4.5H36V0ZM18,9v4.5H0V18H18v4.5l9-6.75Z" fill="#82756a" />
       </svg>
       <p class="fs-3">Login to edit or add mountains</p>
@@ -51,34 +51,40 @@ $result = find_all_mtns();
   <?php } ?>
 </header>
 
-<div class="jumbotron categories row">
+<section>
+  <div class="jumbotron categories row">
 
-  <!---------- category filtering ---------->
-  <div class="col-md-6">
+    <!---------- category filtering ---------->
     <h2>Mountain List</h2>
     <span class="fw-light fs-2">Sort By</span>
-    <ul class="categories-flex">
-      <li><a href="index.php">all Mountains</a></li>
-      <li><a href="index.php?displayby=access&displayvalue=hike">Hiking Access</a></li>
-      <li><a href="index.php?displayby=province&displayvalue=ab">Alberta Mountains</a> </li>
-      <li><a href="index.php?displayby=is_volcano&displayvalue=1">Volcanos</a> </li>
-    </ul>
-  </div>
-  <div class="col-md-6 mtn-height">
-    <?php
-    // echo "<h2>2500m to 5000m high </h2>";
-    // $heightLow = mysqli_query($con, "SELECT * FROM dyl_mountains WHERE height BETWEEN 2500 AND 5000 LIMIT 4");
-    // while ($row = mysqli_fetch_array($heightLow)) {
-    //   $title = $row['title'];
-    //   $mtnId = $row['mtn_id'];
-    //   echo "<div><a href=\"page.php?mtn_id=$mtnId\"><p>" . $title . "</p></a></div>";
-    // }
-    // -------- filter height and render html ----------
-    filter_Height("<h2>3000m to 4000m high </h2>", 3000, 4000);
-    ?>
+    <!-------------SEARCH BY SAME METHOD OF PERSONAL WEBSITE------------------------- --->
+    <!-- make php put info into html of list cards and the filter with js-->
+    <!-- same with other features of list ? height,access, etc. -->
+    <!-- -----OR----- -->
+
+    <div class="sort-list container ">
+      <ul class="d-flex p-3 fs-5 text-light bg-dark justify-content-around">
+        <?php
+        // echo "<h2>2500m to 5000m high </h2>";
+        // $heightLow = mysqli_query($con, "SELECT * FROM dyl_mountains WHERE height BETWEEN 2500 AND 5000 LIMIT 4");
+        // while ($row = mysqli_fetch_array($heightLow)) {
+        //   $title = $row['title'];
+        //   $mtnId = $row['mtn_id'];
+        //   echo "<div><a href=\"page.php?mtn_id=$mtnId\"><p>" . $title . "</p></a></div>";
+        // }
+        // -------- filter height and render html ----------
+        // filter_Height("<h2>3000m to 4000m high </h2>", 3000, 4000);
+        ?>
+        <li>ALL</li>
+        <li>Volcanoes</li>
+        <li>Hike Access</li>
+        <li>Alberta Mountains</li>
+      </ul>
+    </div>
+
 
   </div>
-</div>
+</section>
 
 
 
@@ -109,38 +115,40 @@ $result = find_all_mtns();
 
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <img class="mtn-card-gradient" src="<?php echo "uploads/thumbnails/" .  h($mtnImage) ?>" alt="">
-        </div>
+        <img class="mtn-card-gradient" src="<?php echo "uploads/thumbnails/" .  h($mtnImage) ?>" alt="">
       </div>
-      <div class="thumb col-md-6">
-        <div class="flex row">
-          <?php
-          echo "<h3 class=\"displayCategory\"></span> <span class=\"displayInfo\">" . h($title) . "</h3>\n";
-          echo "<div class=\"displayCategory fs-3 text-secondary\"></span> <span class=\"displayInfo\">" . h($height) . "m</div>\n";
-          ?>
-        </div>
-        <div>
-          <?php echo "<div class=\"displayCategory fs-4 fw-bold text-uppercase\"></span> <span class=\"displayInfo\">" . h($province) . "</div>\n"; ?>
-          <?php echo "<div class=\"displayCategory fs-5 fs-normal\"></span> <span class=\"displayInfo\">" . h($access) . " Access</div>\n"; ?>
-          <?php echo "<div class=\"displayCategory fs-5 fst-italic\"></span> <span class=\"displayInfo\">" . h($volcanoText) . "</div>\n"; ?>
-
-        </div>
-      <?php
-
-
-      echo "<div class=\"displayCategory\"></span> <span class=\"displayInfo\">" . h($description) . "|</div>\n";
-
-      // CREATE A "detail.php" PAGE FOR A SINGLE ITEM VIEW; SHOW ALL INFO
-      echo "<a href=page.php?mtn_id=" .  h(u($mtnId)) . " class='btn btn-secondary'>Details</a><br />";
-
-      echo "</div>";
-    }
-      ?>
+      <div class="d-flex justify-content-between pt-3 pb-3">
+        <?php
+        echo "<h3 class=\"displayCategory\"></span> <span class=\"displayInfo\">" . h($title) . "</h3>\n";
+        echo "<div class=\"displayCategory fs-3 text-secondary\"></span> <span class=\"displayInfo\">" . h($height) . "m</div>\n";
+        ?>
+      </div>
+      <div class="d-flex gap-5 pt-3 pb-3">
+        <?php echo "<div class=\"displayCategory fs-4 fw-bold text-uppercase\"></span> <span class=\"displayInfo\">" . h($province) . "</div>\n"; ?>
+        <?php echo "<div class=\"displayCategory fs-5 fs-normal\"></span> <span class=\"displayInfo\">" . h($access) . " Access</div>\n"; ?>
+        <?php echo "<div class=\"displayCategory fs-5 fst-italic\"></span> <span class=\"displayInfo\">" . h($volcanoText) . "</div>\n"; ?>
 
       </div>
+      <p>
+        <?php echo h($description); ?>
+      </p>
 
-      <?php
+      <a href=<?php echo "\"page.php?mtn_id=" . h(u($mtnId)) . "\"" ?> class='left-auto btn btn-secondary d-block w-25 '>Details</a>
+    <?php
 
-      include(INCLUDES_PATH . "/footer.php");
-      ?>
+
+
+
+
+
+  }
+    ?>
+
+    </div>
+
+</div>
+
+<?php
+
+include(INCLUDES_PATH . "/footer.php");
+?>
